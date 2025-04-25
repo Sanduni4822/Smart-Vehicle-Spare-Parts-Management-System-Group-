6858e7c619cc17e58ea092f04a5d64bfc30d847b
@sanduniwathsala0123 <sanduniwathsala0123@gmail.com>
2025-04-25 19:07:08 +0530

    Fixed footer positioning to appear at bottom of page

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,13 +4,12 @@ import Newsletter from './Newsletter';
 
 export default function Footer() {
   return (
-    <footer className="bg-gray-100 py-8 px-6">
+    <footer className="bg-gray-100 py-8 px-6 fixed bottom-0 left-0 right-0 w-full">
       <div className="max-w-6xl mx-auto flex flex-col md:flex-row justify-between">
         <CompanyInfo />
         <UsefulLinks />
         <Newsletter />
       </div>
-      
     </footer>
   );
 }
