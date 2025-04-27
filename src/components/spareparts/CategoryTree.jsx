497df44e61a1dcf3ccc969817db02d4dda1a315e
@sanduniwathsala0123 <sanduniwathsala0123@gmail.com>
2025-04-27 15:32:54 +0530

    Added sub categories

--- src/components/spareparts/CategoryTree.jsx
+++ src/components/spareparts/CategoryTree.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import CategoryItem from './CategoryItem'; // Importing the child component
+import CategoryItem from './CategoryItem'; // Import the child component
 
 const CategoryTree = () => {
   const [expanded, setExpanded] = useState({});
@@ -36,9 +36,50 @@ const CategoryTree = () => {
         { name: 'Windshields', count: 409 },
       ],
     },
-    { name: 'Automotive Lightings' },
-    { name: 'Tools & Equipment' },
-    { name: 'Performance Parts' },
+    {
+      name: 'Automotive Lightings',
+      subcategories: [
+        { name: 'Headlights', count: 10622 },
+        { name: 'Tail Lights', count: 8085 },
+        { name: 'Turn Signals', count: 6751 },
+        { name: 'Fog Lights', count: 3473 },
+        { name: 'Lighting Components', count: 2094 },
+        { name: 'Interior Lighting', count: 1817 },
+        { name: 'Replacement Bulbs', count: 1737 },
+        { name: 'Custom Lights', count: 335 },
+        { name: 'LED Lights', count: 122 },
+      ],
+    },
+    {
+      name: 'Tools & Equipment',
+      subcategories: [
+        { name: 'Hand Tools & Accessories', count: 7347 },
+        { name: 'Automotive Service Tools', count: 3568 },
+        { name: 'Power Tools & Accessories', count: 1517 },
+        { name: 'Garage & Auto Shop Equipment', count: 1291 },
+        { name: 'Auto Body Shop Tools & Equipment', count: 716 },
+        { name: 'Pneumatic Air Tools', count: 695 },
+        { name: 'Automotive Paints', count: 421 },
+        { name: 'Apparel & Safety Gear', count: 227 },
+        { name: 'Vehicle Lifting Equipment', count: 99 },
+        { name: 'Air Tools & Accessories', count: 28 },
+      ],
+    },
+    {
+      name: 'Performance Parts',
+      subcategories: [
+        { name: 'Brakes & Rotors', count: 12205 },
+        { name: 'Ignition Systems', count: 9757 },
+        { name: 'Cooling Systems', count: 6799 },
+        { name: 'Engine Components', count: 5562 },
+        { name: 'Transmission Systems', count: 4602 },
+        { name: 'Suspension Systems', count: 2896 },
+        { name: 'Exhaust Systems', count: 2720 },
+        { name: 'Fuel Delivery Systems', count: 1904 },
+        { name: 'Steering Systems', count: 1776 },
+        { name: 'Axles & Drivetrain', count: 601 },
+      ],
+    },
     { name: 'Wheels & Tires' },
     { name: 'Interior' },
     { name: 'Exterior' },
@@ -54,7 +95,7 @@ const CategoryTree = () => {
   };
 
   return (
-    <div className="w-full p-4 bg-white rounded-md shadow-md">
+    <div className="w-full p-4 bg-white rounded-md shadow-md overflow-y-auto max-h-screen">
       <h2 className="text-2xl font-bold mb-6 text-gray-800">Categories</h2>
 
       <div className="flex flex-col gap-3">
